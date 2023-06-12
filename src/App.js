90e7bccd9160c1fccf1e6abb3688896d98dadfb6
MAIA CAROLINA SZMEDRA <46872438@ort>
2023-06-12 15:58:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+export default function App() {
+
+
+  //console.log(getRandomInt(10))
+
+  const [pais, setPais] = useState({})
+  const [paises, setPaises] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`https://countriesnow.space/api/v0.1/countries/flag/images`)  // acá hacemos la consulta de axios a la API
+      setPaises(response.data.data) // la data devuelta de la consulta la almacenamos en la variable movie
+      seleccionarPaisRandom(response.data.data)
+    }
+    fetchData() // ejecutamos la función de búsqueda de datos
+
+  },[])
+
+  
+  function seleccionarPaisRandom(paises) {
+    const randomNum =  Math.floor(Math.random()* paises.length);
+    setPais(paises[randomNum])
+  }
+ console.log(pais)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+
+  )
 }
 
-export default App;

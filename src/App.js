12c90bd048ea19ce8a98543b0ce255e3baf52775
MAIA CAROLINA SZMEDRA <46872438@ort>
2023-06-14 12:06:12 -0300

    no funcionan los puntos

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ export default function App() {
 
   const [pais, setPais] = useState({})
   const [paises, setPaises] = useState([])
+  const [paisAdivinado, setPaisAdivinado] = useState('')
+  const [puntos, setPuntos] = useState('')
+  setPuntos(0)
+
 
   useEffect(() => {
     async function fetchData() {
@@ -18,20 +22,52 @@ export default function App() {
     }
     fetchData() // ejecutamos la función de búsqueda de datos
 
-  },[])
+  }, [])
 
-  
+
   function seleccionarPaisRandom(paises) {
-    const randomNum =  Math.floor(Math.random()* paises.length);
+    const randomNum = Math.floor(Math.random() * paises.length);
     setPais(paises[randomNum])
+
   }
- console.log(pais)
+
+  const handleSubmit = (e) => {
+    seleccionarPaisRandom(paises)
+
+    e.preventDefault()
+    if (pais.name.toLowerCase() === paisAdivinado.toLowerCase()) {
+      setPuntos(puntos + 10)
+      alert("Correcto!")
+    }
+    else {
+      setPuntos(puntos - 1)
+      alert("Incorrecto!")
+    }
+  }
 
   return (
     <div>
-      
+      <h1>Guess the flag!</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <img width="500px" height = "auto" src={pais.flag}></img>
+          <div>
+            <input
+              id="nombrePais"
+              type="text"
+              name="nombrePais"
+              placeholder="Nombre país"
+              onChange={(e) => setPaisAdivinado(e.target.value)}
+            />
+            <button type="submit" className="u-full-width button-primary">
+              Guess
+            </button>
+          </div>
+          <h1>You have {puntos} points</h1>
+        </div>
+      </form>
     </div>
-
   )
+
 }
 

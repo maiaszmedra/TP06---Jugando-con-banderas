1af54d42fa2bfbaac3d991df416130687b182f87
LUCIANA WAREMKRAUT <47026652@ort>
2023-06-26 15:57:17 -0300

    godashe

--- src/App.js
+++ src/App.js
@@ -10,30 +10,26 @@ export default function App() {
   const [pais, setPais] = useState({})
   const [paises, setPaises] = useState([])
   const [paisAdivinado, setPaisAdivinado] = useState('')
-  const [puntos, setPuntos] = useState('')
-  setPuntos(0)
+  const [puntos, setPuntos] = useState(0)
+
 
 
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get(`https://countriesnow.space/api/v0.1/countries/flag/images`)  // acá hacemos la consulta de axios a la API
-      setPaises(response.data.data) // la data devuelta de la consulta la almacenamos en la variable movie
-      seleccionarPaisRandom(response.data.data)
+      setPaises(response.data.data) // la data devuelta de la consulta la almacenamos en la variable movie     
     }
     fetchData() // ejecutamos la función de búsqueda de datos
-
   }, [])
 
 
   function seleccionarPaisRandom(paises) {
     const randomNum = Math.floor(Math.random() * paises.length);
     setPais(paises[randomNum])
-
   }
 
   const handleSubmit = (e) => {
-    seleccionarPaisRandom(paises)
-
+    seleccionarPaisRandom(paises)//jessi dijo que iba afuera pero tira error 
     e.preventDefault()
     if (pais.name.toLowerCase() === paisAdivinado.toLowerCase()) {
       setPuntos(puntos + 10)
@@ -46,19 +42,19 @@ export default function App() {
   }
 
   return (
-    <div>
+    <div class="center">
       <h1>Guess the flag!</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <img width="500px" height = "auto" src={pais.flag}></img>
+          <img alt="..." width="500px" height="auto" src={pais.flag}></img>
           <div>
-            <input
-              id="nombrePais"
-              type="text"
-              name="nombrePais"
-              placeholder="Nombre país"
-              onChange={(e) => setPaisAdivinado(e.target.value)}
-            />
+              <input
+                id="nombrePais"
+                type="text"
+                name="nombrePais"
+                required="required"
+                onChange={(e) => setPaisAdivinado(e.target.value)}
+              />
             <button type="submit" className="u-full-width button-primary">
               Guess
             </button>
@@ -70,4 +66,3 @@ export default function App() {
   )
 
 }
-
